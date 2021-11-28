61c31accf5a6ccda636e478f4edef0292352e68e
yunrap <dbsdpwl7@gmail.com>
2021-11-29 00:28:26 +0900

    weather 기능추가

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "d3ca271ca5c317cfd55a8e0f76a081f9";
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lag = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lag}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json().then(data => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerHTML = `<b>지역: ${data.name}<br></b>`;
+        weather.innerHTML = `<b>오늘의 날씨 : ${data.weather[0].main} <br> 온도 : ${data.main.temp}</b>`;
+    }));
+}
+function onGeoError() {
+    alert("위치를 찾을수 없습니다.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+
