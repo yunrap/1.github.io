cb349e2483b953be168d40bf76325c54f7c420da
yunrap <dbsdpwl7@gmail.com>
2021-12-10 16:05:05 +0900

    10미만일때 0표시 추가

--- js/clock.js
+++ js/clock.js
@@ -7,8 +7,7 @@ function myTimer() {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     
-    nowTimePart.innerHTML = `${hours}:${minutes}`;
-
+    nowTimePart.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 }
 
 
